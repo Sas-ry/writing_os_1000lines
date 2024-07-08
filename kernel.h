ace2124f8adb0d4d086dcda9ed3ea45027a1cc9e
Sasamoto-Ryoki <sasamoto.ryoki@gmail.com>
2024-07-09 00:44:44 +0900

    implement process

--- kernel.h
+++ kernel.h
@@ -58,3 +58,14 @@ struct sbiret {
         printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
         while (1) {}                                                          \
     } while (0)                                                               
+
+#define PROCS_MAX     8 // 最大プロセス数
+#define PROC_UNUSED   0 // 未使用のプロセス管理構造体
+#define PROC_RUNNABLE 1 // 実行可能なプロセス
+
+struct process {
+    int pid;             // プロセスID                                                                                                                              
+    int state;           // プロセスの状態                                                                                                
+    vaddr_t sp;          // コンテキストスイッチ時のスタックポインタ
+    uint8_t stack[8192]; // カーネルスタック
+};
